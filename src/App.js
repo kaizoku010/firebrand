9a9f0e6e577ade8780bf8d6144b480bc551c4fd7
Dixon Muneza <dixontheworldvsy@gmail.com>
2021-07-14 01:15:11 +0300

    sitecommit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,196 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import "./App.css"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import me from "./img/pp.jpg"
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <div className="content">
+
+          <div className="container">
+            <div className="row">
+              <div className="col-sm-12">
+                <div className="profile-user-box card-box bg-custom">
+                  <div className="row">
+                    <div className="col-sm-6"><span className="float-left mr-3">
+                      <img src={me} alt="" className="thumb-lg rounded-circle" /></span>
+                      <div className="media-body text-white">
+                        <h4 className="mt-1 mb-1 font-18">Muneza K. Dixon</h4>
+                        <p className="font-13 text-light">Web Developer/ Graphics Designer/ App Developer</p>
+                        <p className="text-light mb-0">Uganda, Kampala</p>
+                      </div>
+                    </div>
+                    <div className="col-sm-6">
+                      <div className="text-right">
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-xl-4">
+                <div className="card-box">
+                  <h4 className="header-title mt-0">Personal Information</h4>
+                  <div className="panel-body">
+                    <p className="text-muted font-13">Howdy, I’m Muneza Dixon
+                      residing in this beautiful electro magnetic world. I build scalable websites, and mobile apps with great UX and UI design. I have worked with companies like Willapps UG, Savage Uganda and The Wellbeing Foundation. Meet me or Contact me for any queries.</p>
+                    <hr></hr>
+                    <div className="text-left">
+                      <p className="text-muted font-13"><strong>Full Name :</strong> <span className="m-l-15">Muneza Dixon</span></p>
+                      <p className="text-muted font-13"><strong>Mobile :</strong><span className="m-l-15">(+256) 753 971 151</span></p>
+                      <p className="text-muted font-13"><strong>Email :</strong> <span className="m-l-15">dixontheworldvsy@gmail.com</span></p>
+                      <p className="text-muted font-13"><strong>Location :</strong> <span className="m-l-15">Uganda</span></p>
+
+                      <p className="text-muted font-13"><strong>Languages :</strong> <span className="m-l-5">
+                        <span className="flag-icon flag-icon-us m-r-5 m-t-0" title="us"></span> <span>java </span>
+                      </span><span className="m-l-5"><span className="flag-icon flag-icon-de m-r-5" title="de"></span>
+                          <span>Javascript</span> </span><span className="m-l-5"><span className="flag-icon flag-icon-es m-r-5" title="es"></span>
+                          <span>HTML/CSS</span> </span><span className="m-l-5">
+                          <span className="flag-icon flag-icon-fr m-r-5" title="fr"></span> <span>C#</span></span>
+                      </p>
+
+                      <p className="text-muted font-13"><strong>Frameworks and Liberies :</strong> <span className="m-l-5">
+                        <span className="flag-icon flag-icon-us m-r-5 m-t-0" title="us"></span> <span>ReactJS </span>
+                      </span><span className="m-l-5"><span className="flag-icon flag-icon-de m-r-5" title="de"></span>
+                          <span>Wordpress</span> </span><span className="m-l-5"><span className="flag-icon flag-icon-es m-r-5" title="es"></span>
+                          <span>MonoBehaviour</span> </span><span className="m-l-5">
+                        </span>
+                      </p>
+                      <p className="text-muted font-13"><strong>Code Base :</strong> <span className="m-l-5">
+                        <span className="flag-icon flag-icon-us m-r-5 m-t-0" title="us"></span>
+                        <Link to="https://github.com/kaizoku010">
+                          <span>
+                            github/kaizoku010
+
+                          </span>
+                        </Link>
+                      </span>
+                      </p>
+
+                    </div>
+                    <ul className="social-links list-inline mt-4 mb-0">
+                      <li className="list-inline-item"><a title="" data-placement="top" data-toggle="tooltip" className="tooltips" href="" data-original-title="Facebook"><i className="fa fa-facebook"></i></a></li>
+                      <li className="list-inline-item"><a title="" data-placement="top" data-toggle="tooltip" className="tooltips" href="" data-original-title="Twitter"><i className="fa fa-twitter"></i></a></li>
+                      <li className="list-inline-item"><a title="" data-placement="top" data-toggle="tooltip" className="tooltips" href="" data-original-title="Skype"><i className="fa fa-skype"></i></a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+              <div className="col-xl-8">
+                <div className="card-box">
+                  <h4 className="header-title mt-0 mb-3">Experience</h4>
+                  <div className="">
+                    <div className="">
+                      <h5 className="text-custom">Lead designer / Developer</h5>
+                      <p className="mb-0">
+                        <Link to="https://play.google.com/store/apps/details?id=com.digitalminds.android.nusu">
+
+                          Nusu
+                        </Link>
+                      </p>
+                      <p><b>2016-2021</b></p>
+                      <p className="text-muted font-13 mb-0">
+                        Nusu is a simple financial assistant that enables you or your business to keep track of its finances,
+                        on what and where your finances comes from by keeping an offline list of various entries.
+                      </p>
+                    </div>
+                    <hr></hr>
+                    <div className="">
+                      <h5 className="text-custom">Lead designer / Developer</h5>
+                      <p className="mb-0">
+                        <Link to="https://play.google.com/store/apps/details?id=com.xoGames.GunsBunnies">
+                          GunBunnies
+                        </Link></p>
+                      <p><b>2020-2021</b></p>
+                      <p className="text-muted font-13 mb-0">Help the noble knight Emma annihilate the evil bunny overlord
+                        and his zombie minions from the Castle.</p>
+                    </div>
+                  </div>
+
+
+                </div>
+                <div className="card-box">
+                  <h4 className="header-title mb-3">My Projects</h4>
+                  <div className="table-responsive">
+                    <table className="table">
+                      <thead>
+                        <tr>
+                          <th>#</th>
+                          <th>Project Name</th>
+                          <th>Start Date</th>
+                          <th>Due Date</th>
+                          <th>Status</th>
+                          <th>Assign</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        <tr>
+                          <td>1</td>
+                          <td><Link to="https://thewellbeingfoundation.net/shop.wuzi/">Wuzi</Link></td>
+                          <td>10/05/2021</td>
+                          <td>14/07/2021</td>
+                          <td><span className="label label-info">Work in Progress</span></td>
+                          <td>The Wellbeing Fondation</td>
+                        </tr>
+                        <tr>
+                          <td>2</td>
+                          <td><Link to="https://thewellbeingfoundation.net">The Wellbeing Foundation</Link></td>
+                          <td>07/02/2020</td>
+                          <td>07/02/2021</td>
+                          <td><span className="label label-success">Work in Progress</span></td>
+                          <td>The Wellbeing Fondation</td>
+                        </tr>
+                        <tr>
+                          <td>3</td>
+                          <td><Link to="http://ngb.go.ug/">National Gaming Board Uganda</Link></td>
+                          <td>05/01/2019</td>
+                          <td>13/01/2019</td>
+                          <td><span className="label label-pink">Done</span></td>
+                          <td>Willapps</td>
+                        </tr>
+                        <tr>
+                          <td>4</td>
+                          <td><Link to="https://regentestatesltd.com//">Regent Estates</Link></td>
+                          <td>01/08/2019</td>
+                          <td>15/08/2019</td>
+                          <td><span className="label label-purple">Done</span></td>
+                          <td>Willapps</td>
+                        </tr>
+                        <tr>
+                          <td>5</td>
+                          <td><Link to="http://mdparchitecture.com/">MDP ARCHITECTURE SARL</Link></td>
+                          <td>02/09/2020</td>
+                          <td>17/09/2020</td>
+                          <td><span className="label label-warning">Coming soon</span></td>
+                          <td>Willapps</td>
+                        </tr>
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+
+        </div>
+      </div>
+
+    </Router>
+
   );
 }
+
 
 export default App;
