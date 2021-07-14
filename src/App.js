f97a0ee5b11e644ccf889a9828af97cc157f815b
Dixon Muneza <dixontheworldvsy@gmail.com>
2021-07-14 03:17:02 +0300

    link fix

--- src/App.js
+++ src/App.js
@@ -69,23 +69,24 @@ function App() {
                       </p>
                       <p className="text-muted font-13"><strong>Code Base :</strong> <span className="m-l-5">
                         <span className="flag-icon flag-icon-us m-r-5 m-t-0" title="us"></span>
-                        <Link to="https://github.com/kaizoku010">
-                          <span>
-                            github/kaizoku010
+                        <a target="_blank" href="https://github.com/kaizoku010">
+                        <span>
+                            Github/kaizoku010
 
                           </span>
-                        </Link>
+                        </a>
+                    
                       </span>
                       </p>
 
                       <p className="text-muted font-13"><strong>Graphics :</strong> <span className="m-l-5">
                         <span className="flag-icon flag-icon-us m-r-5 m-t-0" title="us"></span>
-                        <Link to="https://www.deviantart.com/imfaceless">
+                        <a target="_blank" href="https://www.deviantart.com/imfaceless">
                           <span>
                             Deviant Art
-
-                          </span>
-                        </Link>
+      </span>
+                          </a>
+                        
                       </span>
                       </p>
 
@@ -105,10 +106,11 @@ function App() {
                     <div className="">
                       <h5 className="text-custom">Lead designer / Developer</h5>
                       <p className="mb-0">
-                        <Link to="https://play.google.com/store/apps/details?id=com.digitalminds.android.nusu">
+                      <a target="_blank" href="https://play.google.com/store/apps/details?id=com.digitalminds.android.nusu">
+
 
                           Nusu
-                        </Link>
+                        </a>
                       </p>
                       <p><b>2016-2021</b></p>
                       <p className="text-muted font-13 mb-0">
@@ -120,9 +122,10 @@ function App() {
                     <div className="">
                       <h5 className="text-custom">Lead designer / Developer</h5>
                       <p className="mb-0">
-                        <Link to="https://play.google.com/store/apps/details?id=com.xoGames.GunsBunnies">
+                      <a target="_blank" href="https://play.google.com/store/apps/details?id=com.xoGames.GunsBunnies">
+
                           GunBunnies
-                        </Link></p>
+                        </a></p>
                       <p><b>2020-2021</b></p>
                       <p className="text-muted font-13 mb-0">Help the noble knight Emma annihilate the evil bunny overlord
                         and his zombie minions from the Castle.</p>
@@ -148,7 +151,12 @@ function App() {
                       <tbody>
                         <tr>
                           <td>1</td>
-                          <td><Link to="https://thewellbeingfoundation.net/shop.wuzi/">Wuzi</Link></td>
+                          <td>
+                          <a target="_blank" href="https://thewellbeingfoundation.net/shop.wuzi/">
+
+                        Wuzi
+                            </a>
+                          </td>
                           <td>10/05/2021</td>
                           <td>14/07/2021</td>
                           <td><span className="label label-info">Work in Progress</span></td>
@@ -156,7 +164,12 @@ function App() {
                         </tr>
                         <tr>
                           <td>2</td>
-                          <td><Link to="https://thewellbeingfoundation.net">The Wellbeing Foundation</Link></td>
+                          <td>
+                          <a target="_blank" href="https://thewellbeingfoundation.net">
+
+                            The Wellbeing Foundation
+                            </a>
+                          </td>
                           <td>07/02/2020</td>
                           <td>07/02/2021</td>
                           <td><span className="label label-success">Work in Progress</span></td>
@@ -164,7 +177,12 @@ function App() {
                         </tr>
                         <tr>
                           <td>3</td>
-                          <td><Link to="http://ngb.go.ug/">National Gaming Board Uganda</Link></td>
+                          <td>
+                          <a target="_blank" href="http://ngb.go.ug/">
+
+                            National Gaming Board Uganda
+                            </a>
+                          </td>
                           <td>05/01/2019</td>
                           <td>13/01/2019</td>
                           <td><span className="label label-pink">Done</span></td>
@@ -172,7 +190,12 @@ function App() {
                         </tr>
                         <tr>
                           <td>4</td>
-                          <td><Link to="https://regentestatesltd.com//">Regent Estates</Link></td>
+                          <td>
+                          <a target="_blank" href="https://regentestatesltd.com">
+
+                            Regent Estates
+                            </a>
+                          </td>
                           <td>01/08/2019</td>
                           <td>15/08/2019</td>
                           <td><span className="label label-purple">Done</span></td>
@@ -180,7 +203,12 @@ function App() {
                         </tr>
                         <tr>
                           <td>5</td>
-                          <td><Link to="http://mdparchitecture.com/">MDP ARCHITECTURE SARL</Link></td>
+                          <td>
+                          <a target="_blank" href="http://mdparchitecture.com/">
+
+                          MDP ARCHITECTURE SARL
+                            </a>
+                          </td>
                           <td>02/09/2020</td>
                           <td>17/09/2020</td>
                           <td><span className="label label-warning">Done</span></td>
