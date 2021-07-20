aa479de63c8851ae33d6c5f32408a832aecfba03
Dixon Muneza <dixontheworldvsy@gmail.com>
2021-07-21 00:28:17 +0300

    commit 7

--- src/App.js
+++ src/App.js
@@ -21,8 +21,7 @@ function App() {
             <div className="row">
               <div className="col-sm-12">
                 <div className="profile-user-box card-box bg-custom">
-                  <div className="row">
-                    <div className="col-sm-6"><span className="float-left mr-3">
+                    <div className="holder__"><span className="float-left mr-3">
                       <img src={me} alt="" className="thumb-lg rounded-circle" /></span>
                       <div className="media-body text-white">
                         <h4 className="mt-1 mb-1 font-18">Muneza K. Dixon</h4>
@@ -30,12 +29,7 @@ function App() {
                         <p className="text-light mb-0">Uganda, Kampala</p>
                       </div>
                     </div>
-                    <div className="col-sm-6">
-                      <div className="text-right">
-                      </div>
-                    </div>
-                  </div>
-                </div>
+                                </div>
               </div>
             </div>
             <div className="row">
